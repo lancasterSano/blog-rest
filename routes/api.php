ca142353d64c0073c24ab1454d65a86accfa048e
Alexander Aratovskiy <alex.aratovskiy@gmail.com>
2018-08-26 23:56:57 +0300

    #1 Implement controllers, error handling and route bindings

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('categories', 'CategoryController@index');
+Route::get('posts', 'PostController@index');
+Route::get('posts/{post}', 'PostController@show');
+Route::post('posts', 'PostController@store');
+Route::put('posts/{post}', 'PostController@update');
+Route::delete('posts/{post}', 'PostController@delete');
